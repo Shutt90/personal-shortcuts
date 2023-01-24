bc592192203885e56924b37ee9b255213ad948e1
Liam Pugh <liam.pugh.009@gmail.com>
2023-01-24 19:54:31 +0000

    issue/error reporting

--- main.go
+++ main.go
@@ -9,12 +9,19 @@ import (
 )
 
 func main() {
+	if len(os.Args) == 0 {
+		fmt.Println("not enough arguments")
+		return
+	}
 	switch os.Args[1] {
 	case "youtube":
+		if len(os.Args) <= 1 {
+			fmt.Println("require search parameter")
+			return
+		}
 		query := fmt.Sprintf("https://www.youtube.com/results?search_query=%v", os.Args[2])
 		openBrowser(query)
 	}
-
 }
 
 func openBrowser(url string) bool {
